efb63f3fdd709b16bf0b87a94fbcdf9d41e7f6f0
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-04-28 21:01:06 +0000

    Added Snowboy assets, tweaking gradle build settings

--- android/src/main/java/com/example/flutter_snowboy/FlutterSnowboyPlugin.java
+++ android/src/main/java/com/example/flutter_snowboy/FlutterSnowboyPlugin.java
@@ -14,20 +14,27 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import ai.kitt.snowboy.audio.RecordingThread;
 import ai.kitt.snowboy.audio.PlaybackThread;
+import ai.kitt.snowboy.Constants;
 
 import ai.kitt.snowboy.audio.AudioDataSaver;
+
+import ai.kitt.snowboy.SnowboyDetect;
+
 
 import android.media.AudioManager;
 
 
 /** FlutterSnowboyPlugin */
 public class FlutterSnowboyPlugin implements FlutterPlugin, MethodCallHandler {
+
+  static { System.loadLibrary("snowboy-detect-android"); }
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private RecordingThread recordingThread;
+  private SnowboyDetect detector;
 
   public FlutterSnowboyPlugin() {
 
@@ -55,9 +62,16 @@ public class FlutterSnowboyPlugin implements FlutterPlugin, MethodCallHandler {
         }
     };
 
+  private static final String ACTIVE_RES = Constants.ACTIVE_RES;
+  private static final String ACTIVE_UMDL = Constants.ACTIVE_UMDL;
+
+    private static String strEnvWorkSpace = Constants.DEFAULT_WORK_SPACE;
+    private String activeModel = strEnvWorkSpace+ACTIVE_UMDL;
+    private String commonRes = strEnvWorkSpace+ACTIVE_RES;
 
   public void prepareSnowboy() {
-    recordingThread = new RecordingThread(handle, new AudioDataSaver());
+    detector = new SnowboyDetect(commonRes, activeModel);
+    // recordingThread = new RecordingThread(handle, new AudioDataSaver());
   }
 
   @Override
