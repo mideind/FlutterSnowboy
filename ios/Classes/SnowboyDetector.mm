6fbbedf422200b579f703c9872d1abcb5c533d33
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-05-20 00:18:49 +0000

    Added Apache 2 license header to ObjC src files

--- ios/Classes/SnowboyDetector.mm
+++ ios/Classes/SnowboyDetector.mm
@@ -1,28 +1,19 @@
 /*
- * This file is part of the Embla iOS app
- * Copyright (c) 2019-2021 Miðeind ehf.
- * Author: Sveinbjorn Thordarson
+ * Copyright (C) 2021 Miðeind ehf.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
-
-/*
-    Singleton wrapper class around OpenEars' Pocketsphinx local speech recognition
-    used for hotword activation ("Hæ Embla"/"Hey Embla"). Currently uses an English
-    language acoustic model with custom phonemes. Going forward, this should
-    probably be replaced with a robust local neural network trained on a large set
-    of activation phrase recordings. Reliability is currently much poorer than Siri's.
-*/
 
 #import "Common.h"
 #import "SnowboyDetector.h"
